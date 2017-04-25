aaebae51c3c740f7a69b60fe35198dc989c8cc92
Ilija Rankovic <ilijarankovic@gmail.com>
2017-04-25 19:41:19 +0200

    Uradjen zadatak brisanja vrednosti fixes #10

--- src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
+++ src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -1,20 +1,44 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Vagon;
+import model.Voz;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Created by androiddevelopment on 25.4.17..
  */
 public class Zadatak4BrisanjeVrednosti {
+    static Dao<Voz,Integer> vozDao;
+    static Dao<Vagon,Integer> vagonDao;
+
     public static void main(String[] args) {
         ConnectionSource conn = null;
 
         try {
             conn = new JdbcConnectionSource("jdbc:sqlite:vozVagon.db");
+
+            vozDao = DaoManager.createDao(conn, Voz.class);
+            vagonDao = DaoManager.createDao(conn, Vagon.class);
+
+            List<Vagon> vagon = vagonDao.queryForAll();
+            for(Vagon v:vagon)
+                System.out.println("Vagon = " + v);
+
+            List<Vagon> pronadjenVagon=vagonDao.queryForEq(Vagon.POLJE_NOSIVOST, 10);
+            Vagon zaBrisanje= pronadjenVagon.get(0);
+            vagonDao.delete(zaBrisanje);
+
+            vagon = vagonDao.queryForAll();
+            for(Vagon v:vagon)
+                System.out.println("Vagon = " + v);
+            
         } catch (SQLException e) {
             e.printStackTrace();
         }finally {
