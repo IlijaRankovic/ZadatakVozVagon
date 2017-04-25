97b60187cc6358bf8f5c39beaed141a32bc5acd1
Ilija Rankovic <ilijarankovic@gmail.com>
2017-04-25 18:52:13 +0200

    Uradjena klasa Voz fixes #5

--- src/main/java/model/Voz.java
+++ src/main/java/model/Voz.java
@@ -1,7 +1,69 @@
 package model;
 
+import com.j256.ormlite.dao.ForeignCollection;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.field.ForeignCollectionField;
+import com.j256.ormlite.table.DatabaseTable;
+
 /**
  * Created by androiddevelopment on 25.4.17..
  */
+@DatabaseTable(tableName = "voz")
 public class Voz {
+    public static final String POLJE_OZNAKA = "oznaka";
+    public static final String POLJE_NAZIV = "naziv";
+
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField(columnName = POLJE_OZNAKA, canBeNull = false)
+    private String oznaka;
+    @DatabaseField(columnName = POLJE_NAZIV, canBeNull = false)
+    private String naziv;
+
+    @ForeignCollectionField(foreignFieldName = "voz")
+    ForeignCollection<Vagon> vagon;
+
+    public Voz(){}
+
+    public Voz(String oznaka, String naziv){
+        this.oznaka = oznaka;
+        this.naziv = naziv;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOznaka() {
+        return oznaka;
+    }
+
+    public void setOznaka(String oznaka) {
+        this.oznaka = oznaka;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public ForeignCollection<Vagon> getVagon() {
+        return vagon;
+    }
+
+    public void setVagon(ForeignCollection<Vagon> vagon) {
+        this.vagon = vagon;
+    }
+
+    @Override
+    public String toString() {
+        return "Voz (Id: " + id + ", oznaka: " + oznaka + ", naziv: " + naziv + ')';
+    }
 }
